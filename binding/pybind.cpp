a4b1c732b6b9ca01559cf8f480f74b4655c69b4c
haoyu <heyvrann@gmail.com>
2025-10-20 11:46:22 +0800

    R-Tree implementation prototype

--- binding/pybind.cpp
+++ binding/pybind.cpp
@@ -1,10 +1,26 @@
 #include <pybind11/pybind11.h>
-
-void hello_core();
+#include <pybind11/stl.h>
+#include "../core/rtree.cpp"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(rtse, m) {
     m.doc() = "R-Tree Search Engine core bindings";
-    m.def("hello_core", &hello_core, "Print hello message from C++ core");
+    
+    py::class_<rtse::Point2>(m, "Point2")
+        .def(py::init<double, double>())
+        .def_readwrite("x", &rtse::Point2::x)
+        .def_readwrite("y", &rtse::Point2::y);
+
+    py::class_<rtse::Box2>(m, "Box2")
+        .def(py::init<>())
+        .def_readwrite("min", &rtse::Box2::min)
+        .def_readwrite("max", &rtse::Box2::max);
+
+    py::class_<rtse::RTree>(m, "RTree")
+        .def(py::init<>())
+        .def("insert", &rtse::RTree::insert)
+        .def("erase", &rtse::RTree::erase)
+        .def("update", &rtse::RTree::update)
+        .def("query_range", &rtse::RTree::query_range);
 }
--- core/rtree.cpp
+++ core/rtree.cpp
@@ -1,4 +1,53 @@
 #include <iostream>
+#include <vector>
+#include <memory>
+
+namespace rtse {
+
+struct Point2 {
+    double x, y;
+    Point2() = default;
+    Point2(double x_, double y_): x(x_), y(y_) {};
+};
+
+struct Box2 {
+    Point2 min;
+    Point2 max;
+};
+
+struct Node {
+    bool is_leaf;
+    std::vector<Box2> boxes;
+    std::vector<int> ids;
+    std::vector<std::shared_ptr<Node>> children;
+};
+
+class RTree {
+public:
+    RTree() {
+        root = std::make_shared<Node>();
+        root->is_leaf = true;
+    }
+    void insert(const Box2& box, int id) {
+        std::cout << "[RTree] 'insert' called." << std::endl;
+    }
+
+    void erase(int id) {
+        std::cout << "[RTree] 'erase' called." << std::endl;
+    }
+    void update(int id, const Box2& new_box) {
+        std::cout << "[RTree] 'update' called." << std::endl;
+    }
+    std::vector<int> query_range(const Box2& query_box) {
+        std::cout << "[RTree] 'query_range' called." << std::endl;
+        return root->ids;
+    }
+private:
+    std::shared_ptr<Node> root;
+};
+
+}
+
 
 void hello_core() {
     std::cout << "RTSE core initialized." << std::endl;
