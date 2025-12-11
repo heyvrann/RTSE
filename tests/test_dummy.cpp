44a87843cc8666e5c88ded14410730378dbb0328
haoyu <heyvrann@gmail.com>
2025-12-11 13:51:34 +0800

    remove unused tests

--- tests/test_dummy.cpp
+++
@@ -1,9 +0,0 @@
-#include <cassert>
-#include <iostream>
-
-int main()
-{
-    std::cout << "Dummy test running..." << std::endl;
-    std::cout << "Dummy test passed!" << std::endl;
-    return 0;
-}
